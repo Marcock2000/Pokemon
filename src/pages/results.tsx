111d2e080ecb257caccfa6467eea81c8799ec6c9
Marcock2000 <92387883+Marcock2000@users.noreply.github.com>
2023-04-01 14:44:14 +0300

    improved ranking page

--- src/pages/results.tsx
+++ src/pages/results.tsx
@@ -5,6 +5,8 @@ import { AsyncReturnType } from "@/utils/ts-bs";
 import Image from "next/image";
 import Head from "next/head";
 
+
+
 const getPokemonInOrder = async () => {
   return await prisma.pokemon.findMany({
     orderBy: {
@@ -34,6 +36,21 @@ const generateCountPercent = (pokemon: PokemonQueryResult[number]) => {
   return (VoteFor / (VoteFor + VoteAgainst)) * 100;
 };
 
+const generateCountPositive = (pokemon: PokemonQueryResult[number]) => {
+  const { VoteFor, VoteAgainst } = pokemon._count;
+  if (VoteFor + VoteAgainst === 0) {
+    return 0;
+  }
+  return (VoteFor);
+};
+const generateCountNegative = (pokemon: PokemonQueryResult[number]) => {
+  const { VoteFor, VoteAgainst } = pokemon._count;
+  if (VoteFor + VoteAgainst === 0) {
+    return 0;
+  }
+  return (VoteAgainst);
+};
+
 const PokemonListing: React.FC<{ pokemon: PokemonQueryResult[number], rank: number }> = ({
   pokemon,
   rank,
@@ -53,6 +70,10 @@ const PokemonListing: React.FC<{ pokemon: PokemonQueryResult[number], rank: numb
       </div>
       <div className="pr-4 font-mono">
         {generateCountPercent(pokemon).toFixed(2) + "%"}
+        <br />
+        {generateCountPositive(pokemon) + "😊"}
+        <span>  |  </span>
+        {generateCountNegative(pokemon) + "😢"}
       </div>
       <div className="absolute top-0 left-0 z-20 flex items-center justify-center px-2 font-semibold text-white bg-gray-600 border border-gray-500 shadow-lg rounded-br-md">
         {rank}
@@ -63,14 +84,16 @@ const PokemonListing: React.FC<{ pokemon: PokemonQueryResult[number], rank: numb
 
 const ResultsPage: React.FC<{
   pokemon: PokemonQueryResult;
+  totalVotes: number;
 }> = (props) => {
   return (
     <div className="flex flex-col items-center">
       <Head>
         <title>Cutest Pokemon Results</title>
-
+    
       </Head>
-      <h2 className="text-2xl font-mono p-4">Cuteness Ranking</h2>
+      <h2 className="text-2xl font-mono p-4">Cuteness Ranking</h2>  
+      <div className="justfiy-start text-left font-mono">Total votes: {props.totalVotes.toLocaleString()}</div>
       <div className="flex flex-col w-full max-w-2xl border">
         {props.pokemon
           .sort((a, b) => {
@@ -96,5 +119,9 @@ export default ResultsPage;
 export const getStaticProps: GetServerSideProps = async () => {
   const pokemonOrdered = await getPokemonInOrder();
   const DAY_IN_SECONDS = 600;
-  return { props: { pokemon: pokemonOrdered }, revalidate: DAY_IN_SECONDS };
+  const votes = await prisma.vote.findMany();
+
+  const totalVotes = votes.length;
+  return { props: { pokemon: pokemonOrdered, totalVotes}, 
+            revalidate: DAY_IN_SECONDS };
 };
